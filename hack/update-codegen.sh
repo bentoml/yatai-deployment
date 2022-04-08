efc5d2e3bd3f991e4563e091b294fd561b3cfe9f
yetone <yetoneful@gmail.com>
2022-04-08 20:44:13 +0800

    feat: update codegen

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -15,35 +15,32 @@ APIS_PKG=api
 OUTPUT_PKG=generated/serving
 # group-version such as foo:v1alpha1
 GROUP=serving
-VERSIONS="v1alpha1 v1alpha2"
+VERSION=v1alpha2
+GROUP_VERSION=${GROUP}:${VERSION}
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-${GOPATH}/pkg/mod/k8s.io/code-generator@v0.23.4}
 
 mkdir -p ./${APIS_PKG}/${GROUP}/
 
-for VERSION in ${VERSIONS}; do
-  GROUP_VERSION=${GROUP}:${VERSION}
+ln -sf $PWD/${APIS_PKG}/${VERSION}/ ./${APIS_PKG}/${GROUP}/${VERSION}
 
-  ln -sf $PWD/${APIS_PKG}/${VERSION}/ ./${APIS_PKG}/${GROUP}/${VERSION}
+rm -rf ${OUTPUT_PKG}/{clientset,informers,listers}
 
-  rm -rf ${OUTPUT_PKG}/{clientset,informers,listers}
+echo "Generating clientset for ${GROUP_VERSION}..."
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+#bash "${CODEGEN_PKG}"/generate-groups.sh "client,informer,lister" \
+bash "${CODEGEN_PKG}"/generate-groups.sh all \
+  ${MODULE}/${OUTPUT_PKG} ${MODULE}/${APIS_PKG} \
+  ${GROUP_VERSION} \
+  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt \
+  --output-base "${SCRIPT_ROOT}"
+#  --output-base "${SCRIPT_ROOT}/../../.."
 
-  echo "Generating clientset for ${GROUP_VERSION}..."
-  # generate the code with:
-  # --output-base    because this script should also be able to run inside the vendor dir of
-  #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-  #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-  #bash "${CODEGEN_PKG}"/generate-groups.sh "client,informer,lister" \
-  bash "${CODEGEN_PKG}"/generate-groups.sh all \
-    ${MODULE}/${OUTPUT_PKG} ${MODULE}/${APIS_PKG} \
-    ${GROUP_VERSION} \
-    --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt \
-    --output-base "${SCRIPT_ROOT}"
-  #  --output-base "${SCRIPT_ROOT}/../../.."
-
-  echo "Generating clientset for ${GROUP_VERSION} done."
-done
+echo "Generating clientset for ${GROUP_VERSION} done."
 
 echo "Generating clientset for ${GROUP} done."
 

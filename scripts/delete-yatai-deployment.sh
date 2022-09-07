72b274469fd534e6d79a4985d0bf4941a5c3a790
yetone <yetoneful@gmail.com>
2022-09-07 16:12:03 +0000

    optimize: optimize the prompt in delete script

--- scripts/delete-yatai-deployment.sh
+++ scripts/delete-yatai-deployment.sh
@@ -4,18 +4,17 @@ CURRETN_CONTEXT=$(kubectl config current-context)
 echo -e "\033[01;31mWarning: this will permanently delete all yatai-deployment resources, existing model deployments, docker image data. Note that external docker image will not be deleted.\033[00m"
 echo -e "\033[01;31mWarning: this also means that all resources under the \033[00m\033[01;32myatai\033[00m, \033[00m\033[01;32myatai-deployment\033[00m, \033[00m\033[01;32myatai-builders\033[00m \033[01;31mnamespaces will be permanently deleted.\033[00m"
 echo -e "\033[01;31mCurrent kubernetes context: \033[00m\033[01;32m$CURRETN_CONTEXT\033[00m"
-read -p "Are you sure to delete yatai-deployment in cluster '$CURRETN_CONTEXT'? [y/n] " -n 1 -r
-echo # move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
-read -p "(Double check) Are you sure to delete yatai-deployment in cluster '$CURRETN_CONTEXT'? [y/n] " -n 1 -r
-echo # move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
+
+
+while true; do
+  echo -e -n "Are you sure to delete yatai-deployment in cluster \033[00m\033[01;32m${CURRETN_CONTEXT}\033[00m? [y/n] "
+  read yn
+  case $yn in
+    [Yy]* ) break;;
+    [Nn]* ) exit;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
 
 echo "Uninstalling yatai-deployment helm chart from cluster.."
 set -x

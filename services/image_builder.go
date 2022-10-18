d56e6945a2ddd07384238ab2d9469e5585a9dd4b
Jithin James <jamesjithin97@gmail.com>
2022-10-18 10:06:58 +0530

    fix merge

--- services/image_builder.go
+++ services/image_builder.go
@@ -326,52 +326,6 @@ echo "Done"
 		})
 	}
 
-	dockerFilePath := "/workspace/buildcontext/env/docker/Dockerfile"
-
-	envs := []corev1.EnvVar{
-		{
-			Name:  "DOCKER_CONFIG",
-			Value: "/kaniko/.docker/",
-		},
-	}
-
-	var command []string
-	args := []string{
-		"--context=/workspace/buildcontext",
-		"--verbosity=info",
-		fmt.Sprintf("--dockerfile=%s", dockerFilePath),
-		fmt.Sprintf("--insecure=%v", !opt.DockerRegistry.Secure),
-		fmt.Sprintf("--destination=%s", imageName),
-	}
-
-	podsCli := kubeCli.CoreV1().Pods(kubeNamespace)
-
-	pod = &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      kubeName,
-			Namespace: kubeNamespace,
-			Labels:    kubeLabels,
-		},
-		Spec: corev1.PodSpec{
-			RestartPolicy:  corev1.RestartPolicyNever,
-			Volumes:        volumes,
-			InitContainers: initContainers,
-			Containers: []corev1.Container{
-				{
-					Name:            "builder",
-					Image:           internalImages.Kaniko,
-					ImagePullPolicy: corev1.PullAlways,
-					Command:         command,
-					Args:            args,
-					VolumeMounts:    volumeMounts,
-					Env:             envs,
-					TTY:             true,
-					Stdin:           true,
-				},
-			},
-		},
-	}
-
 	configCmName := "yatai-image-builder-config"
 	configCm, err := kubeCli.CoreV1().ConfigMaps(kubeNamespace).Get(ctx, configCmName, metav1.GetOptions{})
 	configCmIsNotFound := apierrors.IsNotFound(err)

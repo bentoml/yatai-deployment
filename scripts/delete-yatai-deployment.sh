33da2b7897038ee536d9965c9a91d65f15ccd93a
yetone <yetoneful@gmail.com>
2022-11-01 08:58:57 +0000

    fix(scripts): typo

--- scripts/delete-yatai-deployment.sh
+++ scripts/delete-yatai-deployment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CURRETN_CONTEXT=$(kubectl config current-context)
+CURRENT_CONTEXT=$(kubectl config current-context)
 echo -e "\033[01;31mWarning: this will permanently delete all yatai-deployment resources, existing model deployments, docker image data. Note that external docker image will not be deleted.\033[00m"
 echo -e "\033[01;31mWarning: this also means that all resources under the \033[00m\033[01;32myatai\033[00m, \033[00m\033[01;32myatai-deployment\033[00m, \033[00m\033[01;32myatai-builders\033[00m \033[01;31mnamespaces will be permanently deleted.\033[00m"
-echo -e "\033[01;31mCurrent kubernetes context: \033[00m\033[01;32m$CURRETN_CONTEXT\033[00m"
+echo -e "\033[01;31mCurrent kubernetes context: \033[00m\033[01;32m$CURRENT_CONTEXT\033[00m"
 
 
 while true; do
-  echo -e -n "Are you sure to delete yatai-deployment in cluster \033[00m\033[01;32m${CURRETN_CONTEXT}\033[00m? [y/n] "
+  echo -e -n "Are you sure to delete yatai-deployment in cluster \033[00m\033[01;32m${CURRENT_CONTEXT}\033[00m? [y/n] "
   read yn
   case $yn in
     [Yy]* ) break;;

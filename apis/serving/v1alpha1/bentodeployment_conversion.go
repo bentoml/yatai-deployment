2f98bdd0f4c2bb9e65391fa07ff1efe39680a679
yetone <yetoneful@gmail.com>
2022-11-17 09:10:17 +0000

    refactor: integration with yatai-image-builder

--- apis/serving/v1alpha1/bentodeployment_conversion.go
+++ apis/serving/v1alpha1/bentodeployment_conversion.go
@@ -7,21 +7,21 @@ import (
 )
 
 func (src *BentoDeployment) ConvertTo(dstRaw conversion.Hub) error {
-	dst := dstRaw.(*v1alpha3.BentoDeployment)
+	dst := &v1alpha3.BentoDeployment{}
 	dst.ObjectMeta = src.ObjectMeta
 	dst.Spec.BentoTag = src.Spec.BentoTag
 	dst.Spec.Resources = src.Spec.Resources
 	dst.Spec.Autoscaling = src.Spec.Autoscaling
 	dst.Spec.Envs = src.Spec.Envs
-	return nil
+	return dst.ConvertTo(dstRaw)
 }
 
 func (dst *BentoDeployment) ConvertFrom(srcRaw conversion.Hub) error {
-	src := srcRaw.(*v1alpha3.BentoDeployment)
+	src := &v1alpha3.BentoDeployment{}
 	dst.ObjectMeta = src.ObjectMeta
 	dst.Spec.BentoTag = src.Spec.BentoTag
 	dst.Spec.Resources = src.Spec.Resources
 	dst.Spec.Autoscaling = src.Spec.Autoscaling
 	dst.Spec.Envs = src.Spec.Envs
-	return nil
+	return src.ConvertFrom(srcRaw)
 }

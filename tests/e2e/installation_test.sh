630d1911561ada13bdac9b80f126193928bea1a4
yetone <yetoneful@gmail.com>
2022-12-28 14:55:54 +0000

    feat(e2e): show helm release values

--- tests/e2e/installation_test.sh
+++ tests/e2e/installation_test.sh
@@ -6,8 +6,10 @@ kubectl create ns yatai-system
 
 echo "🚀 Installing yatai-image-builder..."
 YATAI_ENDPOINT='empty' bash <(curl -s "https://raw.githubusercontent.com/bentoml/yatai-image-builder/main/scripts/quick-install-yatai-image-builder.sh")
-echo "✅ yatai-image-builder is ready"
+echo "yatai-image-builder helm release values:"
+helm get values yatai-image-builder -n yatai-image-builder
 echo "🚀 Installing yatai-deployment..."
 YATAI_ENDPOINT='empty' USE_LOCAL_HELM_CHART=true IGNORE_INGRESS=true SKIP_METRICS_SERVER=true DOMAIN_SUFFIX='test.com' UPGRADE_CRDS=false bash ./scripts/quick-install-yatai-deployment.sh
-echo "✅ yatai-deployment is ready"
+echo "yatai-deployment helm release values:"
+helm get values yatai-deployment -n yatai-deployment
 

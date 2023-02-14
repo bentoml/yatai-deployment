24a8c23b0c2aecf0b2baf0cee05db320b4ff5c87
bojiang <bojiang_@outlook.com>
2023-02-14 15:54:45 +0800

    filtering sensitive information

--- container_images/bentoml-monitor-exporter/entrypoint.sh
+++ container_images/bentoml-monitor-exporter/entrypoint.sh
@@ -3,4 +3,4 @@ set -x
 set -e
 cd $FLUENTBIT_ENTRYPOINT_HOME
 ./gen-config.pl
-/fluent-bit/bin/fluent-bit -v -c $FLUENTBIT_ENTRYPOINT_HOME/conf/main.conf
+/fluent-bit/bin/fluent-bit -v -c $FLUENTBIT_ENTRYPOINT_HOME/conf/main.conf | grep --line-buffered -v -e "access_token" -e "JWT signature"
